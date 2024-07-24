f0f695f2d31e8d2bca1ccd6f815389cdf6e7de7a
bean00173 <benmead001@gmail.com>
2024-07-24 15:44:14 +1200

    Math Testing Continuation

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -22,6 +22,10 @@ public class PlayerInput : MonoBehaviour, IDragHandler, IEndDragHandler
 
     [HideInInspector] public UnityEvent<DraggedDirection> inputHandled = new UnityEvent<DraggedDirection>();
 
+    Vector2 startPos;
+    float[] segmentBounds = { 22.5f, 67.5f, 112.5f, 157.5f, 202.5f, 247.5f, 292.5f, 292.5f, 337.5f };
+    DraggedDirection input;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,18 +35,23 @@ public class PlayerInput : MonoBehaviour, IDragHandler, IEndDragHandler
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPos = eventData.position;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("TAP");
+        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Vector3 dragVectorDirection = (eventData.position - eventData.pressPosition).normalized;
-        DraggedDirection input = GetDragDirection(dragVectorDirection);
+        //Vector3 dragVectorDirection = (eventData.position - eventData.pressPosition).normalized;
+        //DraggedDirection input = GetDragDirection(dragVectorDirection);
 
         //if (CheckValidParry(input))
         //{
@@ -58,7 +67,50 @@ public class PlayerInput : MonoBehaviour, IDragHandler, IEndDragHandler
         //    //score--;
         //}
 
+
+
+        //float radius = Mathf.Sqrt(Mathf.Pow(eventData.position.x - startPos.x, 2) + Mathf.Pow(eventData.position.y - startPos.y, 2));
+        //float radius = Mathf.Sqrt(Mathf.Pow(eventData.position.x - eventData.pressPosition.x, 2) + Mathf.Pow(eventData.pressPosition.y - startPos.y, 2));
+
+        //Debug.Log($"{eventData.position}, {eventData.pressPosition}, {radius}");
+        //float angle = (180 * (Mathf.Acos(eventData.position.x / radius)) / Mathf.PI);
+
+        ////float angle = Vector2.Angle(eventData.position, new Vector2(0, radius));
+
+        //Debug.Log(angle);
+
+        //if (angle > segmentBounds[7] && angle < segmentBounds[0]) input = DraggedDirection.Up;
+        //else if (angle > segmentBounds[0] && angle < segmentBounds[1]) input = DraggedDirection.RightUp;
+        //else if (angle > segmentBounds[1] && angle < segmentBounds[2]) input = DraggedDirection.Right;
+        //else if (angle > segmentBounds[2] && angle < segmentBounds[3]) input = DraggedDirection.RightDown;
+        //else if (angle > segmentBounds[3] && angle < segmentBounds[4]) input = DraggedDirection.Down;
+        //else if (angle > segmentBounds[4] && angle < segmentBounds[5]) input = DraggedDirection.LeftDown;
+        //else if (angle > segmentBounds[5] && angle < segmentBounds[6]) input = DraggedDirection.Left;
+        //else if (angle > segmentBounds[6] && angle < segmentBounds[7]) input = DraggedDirection.LeftUp;
+
+
+
+        float gradient = (eventData.position.y - eventData.pressPosition.y) / (eventData.position.x - eventData.pressPosition.x);
+
+        if (gradient > 2 || gradient < -2) input = CheckTop(eventData) ? DraggedDirection.Up : DraggedDirection.Down;
+        else if (gradient < 2 && gradient > .5) input = CheckRightSide(eventData) ? DraggedDirection.RightUp : DraggedDirection.LeftDown;
+        else if (gradient < .5 && gradient > -.5) input = CheckRightSide(eventData) ? DraggedDirection.Right : DraggedDirection.Left;
+        else if (gradient < -2 && gradient > -.5) input = CheckRightSide(eventData) ? DraggedDirection.RightDown : DraggedDirection.LeftUp;
+
+        Debug.Log(gradient);
+
+        Debug.Log(input);
         inputHandled.Invoke(input);
+    }
+
+    private bool CheckRightSide(PointerEventData data)
+    {
+        return data.position.x > data.pressPosition.x;
+    }
+
+    private bool CheckTop(PointerEventData data)
+    {
+        return data.position.y > data.pressPosition.y;
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -66,35 +118,35 @@ public class PlayerInput : MonoBehaviour, IDragHandler, IEndDragHandler
 
     }
 
-    private DraggedDirection GetDragDirection(Vector3 dragVector)
-    {
-        //float positiveX = Mathf.Abs(dragVector.x);
-        //float positiveY = Mathf.Abs(dragVector.y);
-        Vector2 quadrant = CheckQuadrant(dragVector.x, dragVector.y);
+    //private DraggedDirection GetDragDirection(Vector3 dragVector)
+    //{
+    //    float positiveX = Mathf.Abs(dragVector.x);
+    //    float positiveY = Mathf.Abs(dragVector.y);
+    //    Vector2 quadrant = CheckQuadrant(dragVector.x, dragVector.y);
 
-        Debug.Log($"{dragVector.x},{dragVector.y} : {quadrant}");
+    //    Debug.Log($"{dragVector.x},{dragVector.y} : {quadrant}");
 
-        //if (Mathf.Abs(dragVector.x) < Mathf.Cos((3 * Mathf.PI) / 8) && Mathf.Abs(dragVector.x) > Mathf.Cos(Mathf.PI / 8) && Mathf.Abs(dragVector.y) < Mathf.Sin((3 * Mathf.PI) / 8) && Mathf.Abs(dragVector.y) > Mathf.Sin(Mathf.PI / 8))
-        //{
+    //    if (Mathf.Abs(dragVector.x) < Mathf.Cos((3 * Mathf.PI) / 8) && Mathf.Abs(dragVector.x) > Mathf.Cos(Mathf.PI / 8) && Mathf.Abs(dragVector.y) < Mathf.Sin((3 * Mathf.PI) / 8) && Mathf.Abs(dragVector.y) > Mathf.Sin(Mathf.PI / 8))
+    //    {
 
-        //}
-        
-
-        // DraggedDirection draggedDir;
-        //if (positiveX > positiveY)
-        //{
-        //    draggedDir = (dragVector.x > 0) ? DraggedDirection.Right : DraggedDirection.Left;
-        //}
-        //else
-        //{
-        //    draggedDir = (dragVector.y > 0) ? DraggedDirection.Up : DraggedDirection.Down;
-        //}
+    //    }
 
 
-        //Debug.Log(draggedDir);
-        // return draggedDir;
-        return DraggedDirection.Up;
-    }
+    //    DraggedDirection draggedDir;
+    //    if (positiveX > positiveY)
+    //    {
+    //        draggedDir = (dragVector.x > 0) ? DraggedDirection.Right : DraggedDirection.Left;
+    //    }
+    //    else
+    //    {
+    //        draggedDir = (dragVector.y > 0) ? DraggedDirection.Up : DraggedDirection.Down;
+    //    }
+
+
+    //    Debug.Log(draggedDir);
+    //    return draggedDir;
+    //    return DraggedDirection.Up;
+    //}
 
     private Vector2 CheckQuadrant(float x, float y)
     {
