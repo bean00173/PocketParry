2b6ea87685540062a39ac4c0956476b8d8cd5db6
bean00173 <113889619+bean00173@users.noreply.github.com>
2024-09-03 16:51:33 +1200

    New Slider UI + Enemy Behaviour Script Comments

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -31,7 +31,7 @@ public class EnemyBehaviour : MonoBehaviour
     float elapsedTime;
     float parryStart, parryEnd;
 
-    public Slider slider;
+    public Slider slider; // CHANGE LATER to static instance of UI manager for references rather than inspector
     public AttackInformation attackInformation;
     public Enemy enemyStats;
 
@@ -49,7 +49,7 @@ public class EnemyBehaviour : MonoBehaviour
     void Start()
     {
         ac = this.GetComponent<Animator>();
-        PlayerInput.Instance.inputHandled.AddListener(Parry);
+        PlayerInput.Instance.inputHandled.AddListener(Parry); // add listener to input event
 
         StartCoroutine(CooldownTimer(2.0f));
     }
@@ -57,57 +57,46 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canAttack)
+        if (canAttack) // if the enemy can attack
         {
-            canAttack = false;
+            canAttack = false; // prevent from doing so again
 
-            doCombo = DoChanceCalculation(this.enemyStats.comboChance);
-            ac.SetBool("Combo", doCombo);
-            PlayAttack(0);
+            doCombo = DoChanceCalculation(this.enemyStats.comboChance); // check if the attack can combo
+            ac.SetBool("Combo", doCombo); // update animator 
+            PlayAttack(SelectAttack()); // select attack
 
-            if (!doCombo)
+            if (!doCombo) // if not comboing, automatically begin the cooldown timer
             {
                 StartCoroutine(CooldownTimer(5.0f / enemyStats.atkSpeed));
             }
             else
             {
-                comboLength = Random.Range(enemyStats.comboLengthMin, enemyStats.comboLengthMax);
+                comboLength = Random.Range(enemyStats.comboLengthMin, enemyStats.comboLengthMax); // choose a random combo length between variables min and max
             }
 
-            currentState = EnemyState.Attacking;
+            currentState = EnemyState.Attacking; // update state
         }
 
-        if (currentState == EnemyState.Attacking && comboProgress == comboLength && comboProgress > 0)
+        if (currentState == EnemyState.Attacking && comboProgress == comboLength && comboProgress > 0) // if the enemy is attacking and the combo has reached its end
         {
-            Debug.Log($"COMBO DONE, Can Attack? {canAttack}");
-            ac.SetBool("Combo", false);
-            StartCoroutine(CooldownTimer(5.0f / enemyStats.atkSpeed));
+            ac.SetBool("Combo", false); // update animator
+            StartCoroutine(CooldownTimer(5.0f / enemyStats.atkSpeed)); // do cooldown
         }
-
-        Debug.Log($"Combo Progress : {comboProgress} / {comboLength}");
     }
 
-    private void AttackReady()
+    public void PlayAttack(int num) // plays an attack with the designated number
     {
-        canAttack = true;
-        doCombo = false;
-        comboLength = 0;
-        comboProgress = 0;
-    }
-
-    public void PlayAttack(int num)
-    {
-        ac.SetFloat("AttackNumber", num);
+        ac.SetFloat("AttackNumber", num); // set relevant ac params using designated atk number
         ac.SetFloat("TransitionNumber", num);
         ac.SetTrigger("Attack");
     }
 
-    public void Vulnerable()
+    public void Vulnerable() // animation event driven method for start of parry period
     {
-        vulnerable = true;
-        parryStart = GetCurrentAnimatorTime();
+        vulnerable = true; // set vulnerable bool
+        parryStart = GetCurrentAnimatorTime(); // set start time var
         
-        foreach(AttackInfo info in attackInformation.attackInfo)
+        foreach(AttackInfo info in attackInformation.attackInfo) // for each potential attack, cross reference to check with current attack to retrieve attack data
         {
             AnimatorClipInfo[] clipInfo = ac.GetCurrentAnimatorClipInfo(0);
             if (clipInfo[0].clip.name == info.clip.name)
@@ -119,93 +108,101 @@ public class EnemyBehaviour : MonoBehaviour
 
     }
 
-    public void InVulnerable()
+    public void InVulnerable() // animation event driven method for end of parry period
     {
-        vulnerable = false;
-        parryEnd = GetCurrentAnimatorTime();
+        vulnerable = false; // set vulnerable bool
+        parryEnd = GetCurrentAnimatorTime(); // set end time var
 
-        slider.minValue = parryStart;
+        slider.minValue = parryStart; // set slider values for UI
         slider.maxValue = parryEnd;
         slider.value = elapsedTime;
     }
 
-    public float GetCurrentAnimatorTime()
+    public float GetCurrentAnimatorTime() // utility method for returning the current time in the animator
     {
-        return ac.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        return ac.GetCurrentAnimatorStateInfo(0).normalizedTime; 
     }
 
-    public void Parry(ParryDirection dir)
+    public void Parry(ParryDirection dir) // method that listens for player inputs
     {
-        if (vulnerable && CheckInputMatch(dir))
+        if (vulnerable && CheckInputMatch(dir)) // checks for parry conditionals
         {
             Debug.Log("Parried");
-            elapsedTime = GetCurrentAnimatorTime();
-            vulnerable = false;
+            elapsedTime = GetCurrentAnimatorTime(); // sets parry time
+            vulnerable = false; // makes invulnerable
         }
         else
         {
-            elapsedTime = 0;
+            elapsedTime = 0; // if not a parry make sure slider doesnt update
         }
     }
 
-    private bool CheckInputMatch(ParryDirection dir)
+    private bool CheckInputMatch(ParryDirection dir) // utility method to check if input direction matches the required direction in current attack info
     {
-        return dir == currentClipInfo.parryDirection;
+        return dir == currentClipInfo.parryDirection; 
 
     }
 
-    private IEnumerator CooldownTimer(float time)
+    private IEnumerator CooldownTimer(float time) // cooldown timer for after an attack has been executed, preventing attacks too soon after
     {
         currentState = EnemyState.Idle;
-        yield return new WaitForSeconds(time);
+        yield return new WaitForSeconds(time); // LOOK AT LATER - Will probably add randomisation
         AttackReady();
     }
 
-    private bool DoChanceCalculation(float chance)
+    private void AttackReady() // calls when cooldown is complete
+    {
+        canAttack = true; // resets a bunch of variables to prepare for new attacks
+        doCombo = false;
+        comboLength = 0;
+        comboProgress = 0;
+    }
+
+    private bool DoChanceCalculation(float chance) // utility method for calculating random chance
     {
         return Random.value <= chance;
     }
 
-    private int SelectAttack()
+    private int SelectAttack() // method that selects which attack to play
     {
-        List<AttackInfo> potentialAttacks = new List<AttackInfo>();
+        List<AttackInfo> potentialAttacks = new List<AttackInfo>(); // create list var
 
-        foreach(AttackInfo attack in attackInformation.attackInfo)
+        foreach(AttackInfo attack in attackInformation.attackInfo) // cycle through each attack in the current attack info list
         {
-            if (doCombo)
+            if (doCombo) // if the attack is to start a combo, it must be an attack that is able to combo (bool available in attackinfo)
             {
                 if (attack.canCombo) potentialAttacks.Add(attack);
             }
             else
             {
-                potentialAttacks.Add(attack);
+                potentialAttacks.Add(attack); // if not comboing just add the attack to the list (will add all)
             }
         }
 
-        return Random.Range(0, potentialAttacks.Count);
+        return Random.Range(0, potentialAttacks.Count); // out of the new list select a random value
 
     }
 
-    public void UpdateTransitionNumber()
+    public void UpdateTransitionNumber() // animation event driven method for updating which transition will need to play after the current attack
     {
-        int acNum = (int)ac.GetFloat("TransitionNumber");
+        int acNum = (int)ac.GetFloat("TransitionNumber"); // return current values
         int acAtkNum = (int)ac.GetFloat("AttackNumber");
-        if (comboProgress > 0) acNum = acAtkNum; ac.SetFloat("TransitionNumber", acNum);
-        comboProgress++;
+        if (comboProgress > 0) acNum = acAtkNum; ac.SetFloat("TransitionNumber", acNum); // set new values
+        comboProgress++; // update combo progress
     }
 
-    public void UpdateAttackNumber()
+    public void UpdateAttackNumber() // animation event drive method for updating which attack will need to play after the current transition
     {
-        int acNum = (int)ac.GetFloat("AttackNumber");
-        if (acNum == 0 || acNum % 2 == 0)
+        int acNum = (int)ac.GetFloat("AttackNumber"); // return current attack value
+        if (acNum == 0 || acNum % 2 == 0) // since combos are in pairs in the attack data, check if divisible by 2 (exc. 0) 
         {
             acNum++;
-            ac.SetFloat("AttackNumber", acNum);
+            ac.SetFloat("AttackNumber", acNum); // if conditions met move to the next attack
         }
         else
         {
             acNum--;
-            ac.SetFloat("AttackNumber", acNum);
+            ac.SetFloat("AttackNumber", acNum); // if not move back to the previous attack 
         }
     }
 
