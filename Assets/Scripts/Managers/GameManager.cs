8c08688bec65edb4315476e6c08a470d8782f349
bean00173 <benmead001@gmail.com>
2024-10-16 15:45:35 +1300

    frame cap

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -60,7 +60,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
